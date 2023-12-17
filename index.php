dd7d72011251a7d5a5b75da9b4dd37d667cd95bd
Dohan Rizqi Hadityo <95571346+drhadityo@users.noreply.github.com>
2023-12-17 16:51:28 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,119 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home</title>
+    <link rel="stylesheet" href="index.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+
+</head>
+<body>
+    <div class="header" id="header">
+        <div class="container">
+            <div class="navbar">
+                <div class="navL">
+                    <a href="#">
+                        <img src="lain/image/anteroDigital-black.png" alt="">
+                    </a>
+                </div>
+                <div class="navR">
+                        <li><a href="#">Home</a></li>
+                        <li><a href="#kontak">Contact</a></li>
+                        <li><a href="#">About Web</a></li>
+                        <div class="login">
+                            <a href="./lain/loginCus.php">Login Customer</a>
+                            <div class="regis">
+                                <a href="./lain/loginAd.php">Login Admin</a>
+                            </div>
+                          </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="topwrapper">
+        <div class="container">
+            <div class="topmain">
+                <div class="slogan">
+                    <h1>Antero Digital Banking</h1>
+                    <p>Sebuah objek royal yang dapat dinikmati nasabah bank untuk melakukan transaksi perbankan melalui jaringan internet kapan saja dan dimana saja</p>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="alamat">
+        <h3>Kantor pusat</h3>
+        <p>PT. Unijoyo Informatika Tbk.</p>
+        <div class="aldetail">
+            <img src="lain/image/gpsPutih.png" alt="">
+            <p>Jl. Raya Telang, Perumahan Telang Inda, Telang, Kec. Kamal, Kabupaten Bangkalan, Jawa Timur 69162</p>
+        </div>
+    </div>
+    <div class="transisi">
+
+    </div>
+    <div class="midwrapper">
+        <div class="container">
+            <div class="keunggulan">
+                <h1>KE<span>UNGGUL</span>AN</h1>
+                <div class="wrapbox">
+                    <div class="boxunggul">
+                        <h3>Mudah</h3>
+                        <p>Cukup melakukan registrasi Internet Banking ADB di ATM ADB dan registrasi unit kerja ADB terdekat</p>
+                    </div>
+                    <div class="boxunggul">
+                        <h3>User Friendly</h3>
+                        <p>Desain mudah dimengerti dan digunakan, sehingga Anda tidak perlu membaca buku manual untuk menggunakan menu-menu dasar yang ada.</p>
+                    </div>
+                    <div class="boxunggul">
+                        <h3>Dapat dilakukan Setiap Saat</h3>
+                        <p>Penggunaan Internet Banking dimaksudkan untuk memudahkan Anda melakukan transaksi perbankan pada berbagai situasi dan keadaan</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="mid">
+            <h1>INFORMASI <span>UMUM</span></h1>
+            <div class="botwrapper">
+                <div class="container">
+                    <div class="informasi">
+                        
+                        <p>Layanan melakukan transaksi perbankan melalui jaringan internet. Merupakan kegiatan perbankan yang memanfaatkan teknologi internet sebagai media untuk melakukan transaksi dan mendapatkan informasi lainnya melalui website milik bank.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="bot">
+            <div class="container">
+                <div class="fasilitas">
+                    <h2>FASILITAS</h2>
+                    <ul>
+                        <li>Informasi Rekening</li>
+                        <li>Informasi Saldo</li>
+                        <li>Mutasi Rekening</li>
+                        <li>Transfer Dana</li>
+                        <li>Transfer Antar Rek ADB</li>
+                        <li>Lihat Status</li>
+                        <li>Menu Management (Ubah user ID, password, dan email)</li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="footer">
+        <div class="container">
+            <div class="foot">
+                <div class="kontak" id="kontak">
+                    <li><a href="wa.me/6283129939682">Whatsapp</a></li>
+                    <li><a href="mailto:uzaki4167@gmail.com">Mail</a></li>
+                </div>
+                <p>Aset anda yg istimewa akan tersimpan disuatu objek yg terpecaya</p>
+                <img src="./lain/image/anteroDigital-white.png" alt="">
+            </div>
+        </div>
+    </div>
+</body>
+</html>
